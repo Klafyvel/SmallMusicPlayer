95f1d8cb5bfe1f0dc842f8fd9f2b6fceff98cefd
Klafyvel <klafyvel@gmail.com>
2014-05-08 10:51:40 +0200

    The Parser is now able to save the input_str into a list and detects if an instruction is forbidden.

--- notes.py
+++ notes.py
@@ -1,34 +1,34 @@
 #! /usr/bin/python3
 
-NOTES=[
-	"Do",
-	"Do#",
-	"Re",
-	"Re#",
-	"Mi",
-	"Fa",
-	"Fa#",
-	"Sol",
-	"Sol#",
-	"La",
-	"La#",
-	"Si",
-	"Do2",
-	"Do2#",
-	"Re2",
-	"Re2#",
-	"Mi2",
-	"Fa2",
-	"Fa2#",
-	"Sol2",
-	"Sol2#",
-	"La2",
-	"La2#",
-	"Si2",
-	"Silence",
-]
+NOTES={
+	"Do":131,
+	"Do#":139,
+	"Re":147,
+	"Re#":156,
+	"Mi":165,
+	"Fa":175,
+	"Fa#":185,
+	"Sol":196,
+	"Sol#":208,
+	"La":220,
+	"La#":233,
+	"Si":247,
+	"Do2":262,
+	"Do2#":277,
+	"Re2":294,
+	"Re2#":311,
+	"Mi2":330,
+	"Fa2":349,
+	"Fa2#":370,
+	"Sol2":392,
+	"Sol2#":415,
+	"La2":440,
+	"La2#":466,
+	"Si2":494,
+	"Silence":0,
+}
 #temps en millisecondes
-TEMPS={
+DUREE={
 	"croche":250,
 	"noire":500,
 	"blanche":1000,
@@ -46,9 +46,56 @@ class Note:
 			self.nuance = nuance
 
 	def __str__(self):
-		return '({} {} {})'.format(self.note, self.duree, self.nuance)
+		return '({}, {}, {})'.format(self.note, self.duree, self.nuance)
 
-	def tuple(self)
+	def tuple(self):
 		return (self.note, self.duree, self.nuance)
 
 silence = lambda duree: Note(NOTES["Silence"], duree)
+SILENCES = {
+	'-': silence(DUREE['croche']),
+	'--': silence(DUREE['noire']),
+	'---': silence(DUREE['blanche']),
+}
+
+class Partition(list):
+	def __str__(self):
+		s = str()
+		for idx, n in enumerate(self):
+			s += '{}:\t{}\n'.format(idx,n) 
+		return s
+
+
+class ParseError(Exception):
+	pass
+
+class Parser:
+	def __init__(self, partition, input_str):
+		self.partition = partition
+		self.input_str = input_str
+		self.current_note = silence
+
+	def is_allowed_instr(self, word):
+		if not word in NOTES and not word in DUREE and not word in NUANCES and not word in SILENCES:
+			raise ParseError("'{}' is not an allowed word".format(word))
+		return True
+
+	def get_instr_list(self):
+		instr_list = []
+		current_word = ''
+		for c in self.input_str:
+			if (c == ' ' or c== '\n') and not current_word == '':
+				self.is_allowed_instr(current_word)
+				instr_list.append(current_word)
+				current_word = ''
+			elif c != ' ':
+				current_word += c
+		return instr_list
+
+
+	def parse(self):
+		return str(self.get_instr_list())
+
+if __name__ == '__main__':
+	part = Partition()
+	print(Parser(part, 'Si Do2 Do2# --  forte croche Si Do2').parse())
